912bcfea025d0c1c0346693488c8f795d9b20aa2
senethmendis <mendis.seneth@gmail.com>
2024-10-19 23:31:18 +0530

    geo timer added

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import BgMusicFX from "../components/BgMusicFX";
+import WorldTimer from "../components/WorldTimer";
 
 const Layout = () => {
   return (
     <main className="max-w-[1024px] my-auto mx-auto h-[1000px]">
       <BgMusicFX />
       <Outlet />
+      <WorldTimer />
     </main>
   );
 };
